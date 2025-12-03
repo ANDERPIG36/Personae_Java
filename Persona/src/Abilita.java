c71018bda97bee87ac1bbd0285ee1b3adc5d3101
ANDERPIG36 <andreaarinci4@gmail.com>
2025-12-03 11:58:06 +0100

    aggiunte variabili varie

---
+++ Persona/src/Abilita.java
@@ -0,0 +1,24 @@
+public class Abilita {
+    String nome;
+
+    boolean passiva;
+    /*
+    indice array = tipi:
+    0:fisico
+    1:arma da fuoco
+    2:fuoco
+    3:ghiaccio
+    4:elettricita
+    5:vento
+    6:psico
+    7:nucleare
+    8:sacro
+    9:maledizione
+    10:divino
+    11:cura
+    12:divino
+    */
+    int tipo;
+
+
+}
--- Persona/src/Persona.java
+++ Persona/src/Persona.java
@@ -1,9 +1,52 @@
 public class Persona {
     String nome;
+    //arcano
+
+    int exp;
     int livello;
+
     int forza;
     int magia;
     int resistenza;
     int agilita;
     int fortuna;
+
+    /*
+    debolezze:
+    0:nessuna
+    1:debole
+    2:resiste
+    3:assorbe
+    4:blocca
+    5:rifette
+    */
+
+    /*
+    indice array = tipi:
+    0:fisico
+    1:arma da fuoco
+    2:fuoco
+    3:ghiaccio
+    4:elettricita
+    5:vento
+    6:psico
+    7:nucleare
+    8:sacro
+    9:maledizione
+    */
+    int[] debolezza;
+
+    /*int debolezzaFisico;
+    int debolezzaArmaDaFuoco;
+    int debolezzaFuoco;
+    int debolezzaGhiaccio;
+    int debolezzaElettricita;
+    int debolezzaVento;
+    int debolezzaPsico;
+    int debolezzaNucleare;
+    int debolezzaSacro;
+    int debolezzaMaledizione;*/
+
+    //8 slot abilita
+
 }
